186093855304eabfbe151965c46e353d30212113
Nguyễn Quân <emtenquan012@gmail.com>
2025-01-15 16:26:38 +0700

    Create Project, Models, Entity Framework, Controllers, DTOs, POST

---
+++ api/Models/Stock.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    [Table("Stock")]  //chỉ rõ tên bảng trong DB
+    //Mặc định, Entity Framework sử dụng quy tắc số nhiều (pluralization) để đặt tên bảng. Nếu lớp là Stock, EF sẽ tìm bảng Stocks
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
