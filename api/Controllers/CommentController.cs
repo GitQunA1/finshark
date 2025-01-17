cc2135fa3dbcd27168fb7a7327fe1a602e69dc3a
Nguyễn Quân <emtenquan012@gmail.com>
2025-01-17 23:38:07 +0700

    Comment GET + Include() (tải thêm 2 gói package: Newtonsoft.Json và Microsoft.AspNetCore.Mvc.NewtonsoftJson)

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -23,10 +23,21 @@ namespace api.Controllers
         public async Task<IActionResult> GetAll()
         {
             var comments = await _commentRepo.GetAllAsync();
-
             var CommentDto = comments.Select(s => s.ToCommentDto());
             
             return Ok(CommentDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var comment = await _commentRepo.GetByIdAsync(id);
+            if(comment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(comment.ToCommentDto());
         }
     }
 }
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c420b5e83527897879c394ba6ad678913ff1045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c9e4b7906c614a23d89f2ec4fa341b9adbd2b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
