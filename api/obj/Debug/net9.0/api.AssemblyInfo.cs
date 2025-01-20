726df7d479ea6bf1615cc4ed00a24c4592f8cb3d
Nguyễn Quân <emtenquan012@gmail.com>
2025-01-20 11:00:21 +0700

    Comment DELETE

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1270081bf8d6f84f3a1fdbf21248071ca63b4ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e51b44ed3526d443736baa8d7d79d1a70c4f933")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
