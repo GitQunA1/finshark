da0bd96efe63c9dd56276e4cf73591db27d94f79
nguyenhongquan <emtenquan012@gmail.com>
2025-02-09 16:31:27 +0700

    Data Validation and Filtering

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -24,19 +25,25 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll() 
+        public async Task<IActionResult> GetAll([FromQuery] QueryObject query) 
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             //var stocks = await _context.Stocks.ToListAsync();
-            var stocks = await _stockRepo.GetAllAsync();
+            var stocks = await _stockRepo.GetAllAsync(query);
 
             var stockDto = stocks.Select(s => s.ToStockDto());
 
             return Ok(stocks);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             //var stock = await _context.Stocks.FindAsync(id);  //ở đây có thể dùng First or FirstOrDefault
             var stock = await _stockRepo.GetByIdAsync(id);
             if(stock == null)
@@ -44,12 +51,15 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(stock.ToStockDto());
+            return Ok(stock.ToStockDto());  
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var stockModel = stockDto.ToStockFromCreateDTO();
             //await _context.Stocks.AddAsync(stockModel);
             //await _context.SaveChangesAsync();
@@ -59,9 +69,12 @@ namespace api.Controllers
         }
 
         [HttpPut]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             //var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
             var stockModel = await _stockRepo.UpdateAsync(id, updateDto);
             if(stockModel == null)
@@ -73,9 +86,12 @@ namespace api.Controllers
         }
 
         [HttpDelete]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+                
             //var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
             var stockModel = await _stockRepo.DeleteAsync(id);
             if(stockModel == null)
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e51b44ed3526d443736baa8d7d79d1a70c4f933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726df7d479ea6bf1615cc4ed00a24c4592f8cb3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
