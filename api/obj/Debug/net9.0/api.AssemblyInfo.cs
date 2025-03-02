56fed802fdb28eac4c51ac9ff774aee61469d33d
nguyenhongquan <emtenquan012@gmail.com>
2025-03-02 10:39:36 +0700

    Login, AddSwaggerGen

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef5f89fbe3ec9a2c9c416bd3c1f24f8b58c74ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f9599cbb0ec8d157de9339f7a41200cd00992f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
