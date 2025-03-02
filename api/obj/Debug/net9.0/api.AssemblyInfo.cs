901031cb65f90b5fcb720eb2f60bf4779739bd29
nguyenhongquan <emtenquan012@gmail.com>
2025-03-02 21:54:08 +0700

    Many To Many
    - Viết thêm bảng Portfolios để liên kết do mqh nhiều nhiều
    - Xóa Migrations và DB, chạy lại lệnh

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f9599cbb0ec8d157de9339f7a41200cd00992f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56fed802fdb28eac4c51ac9ff774aee61469d33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
